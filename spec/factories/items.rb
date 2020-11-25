a0debbcca5c3f57032d9b676dd0ef3581728a551
YamadaMasao-3d <73803047+YamadaMasao-3d@users.noreply.github.com>
2020-11-25 15:01:08 +0900

    商品一覧表示機能の実装

--- spec/factories/items.rb
+++ spec/factories/items.rb
@@ -1,17 +1,16 @@
 FactoryBot.define do
   factory :item do
-
     after(:build) do |item|
       item.image.attach(io: File.open('public/images/test_image.png'), filename: 'test_image.png')
     end
-    product_name          {"まさおまさお"}
-    product_description   {"いい仕事していますね"}
-    category_id           {1}
-    product_condition_id  {1}
-    shipping_charge_id    {1}
-    shipping_area_id      {1}
-    days_to_ship_id       {1}
-    product_price         {3000}
-    association :user 
+    product_name          { 'まさおまさお' }
+    product_description   { 'いい仕事していますね' }
+    category_id           { 1 }
+    product_condition_id  { 1 }
+    shipping_charge_id    { 1 }
+    shipping_area_id      { 1 }
+    days_to_ship_id       { 1 }
+    product_price         { 3000 }
+    association :user
   end
 end
